351cd26197eea3634c909a01c723adbc1c6cdce1
whaenel <vonhenninghausen@web.de>
2018-10-20 21:13:14 +0200

    HeldTest angefangen

--- eVerlies/src/test/java/de/nikolauspflege/bbw/fia/eVerlies/HeldTest.java
+++ eVerlies/src/test/java/de/nikolauspflege/bbw/fia/eVerlies/HeldTest.java
@@ -2,17 +2,56 @@ package de.nikolauspflege.bbw.fia.eVerlies;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class HeldTest {
 
+	Held held = new Held("Waschlappen");
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
 	@Test
 	void testHeld() {
-		fail("testich mal");
+		assertTrue(held.isAlive(), "Held sollte am anfang leben");
+		assertEquals(0,held.getVermögen(),"Held hat am anfang kein Geld");
+		assertEquals("Waschlappen", held.getName(), "Held sollte seinen Namen Beibehalten");
+	}
+
+	@Test
+	void testSetztAus() {
+		held.mussAussetzen(1);
+		assertEquals(true,held.setztAus(), "held sollte aussetzen");
+	}
+
+	@Test
+	void testGreiftAn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsAlive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testStirbt() {
+		fail("Not yet implemented");
 	}
 
 	@Test
 	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMussAussetzen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetVermögen() {
 		fail("Not yet implemented");
 	}
 
@@ -22,7 +61,22 @@ class HeldTest {
 	}
 
 	@Test
+	void testAddAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
 	void testRemoveSchatz() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testHatVermoegen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testZiehtSichZurueck() {
 		fail("Not yet implemented");
 	}
 
