4ef0485a4b54ec923b1f16b78550c248a5695076
whaenel <vonhenninghausen@web.de>
2018-10-20 21:15:00 +0200

    heldTest angelegt

---
+++ eVerlies/src/test/java/de/nikolauspflege/bbw/fia/eVerlies/HeldTest.java
@@ -0,0 +1,29 @@
+package de.nikolauspflege.bbw.fia.eVerlies;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HeldTest {
+
+	@Test
+	void testHeld() {
+		fail("testich mal");
+	}
+
+	@Test
+	void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVerliertAlles() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveSchatz() {
+		fail("Not yet implemented");
+	}
+
+}
