adae87bac531b92040e3db15de7d6e00e9626850
whaenel <vonhenninghausen@web.de>
2018-10-20 14:32:04 +0200

    held kann Rückzug

--- eVerlies/src/main/java/de/nikolauspflege/bbw/fia/eVerlies/Held.java
+++ eVerlies/src/main/java/de/nikolauspflege/bbw/fia/eVerlies/Held.java
@@ -152,4 +152,58 @@ public class Held {
 >>>>>>> a5ca8ce Alle Rückzugs-aktionen und alle Schätze des Monsters
 	}
 
+	public void ziehtSichZurueck(Monster monster, List<Schatz> vermögen) {
+		// TODO Auto-generated method stub
+		int rettungsWurf = w1.wuerfeln() + w2.wuerfeln();
+		switch (rettungsWurf) {
+		case 7:
+		case 11:
+			// nichts passiert 
+			System.out.println("Der Held hat Glück, denn das Monster schlägt daneben");
+			break;
+		case 6:
+		case 8:
+			// verlust einer Schatzkarte 
+			if (vermögen.isEmpty()) {
+				System.out.println("Der Held hat noch keine Schätze und kann daher auch nichts verlieren! Glück gehabt!");
+			} else {
+				// verlust des 1. Schatzes
+				Schatz verlust = vermögen.remove(0);
+				System.out.println("Der Held verliert eine Schatzkarte im Wert von " + verlust.getValue() + " Goldstücken");
+				monster.add(verlust);
+			}
+			break;
+		case 4:
+		case 5:
+		case 9:
+		case 10:
+			// verlust von 2 schatzkarten, ein mal aussetzen 
+			if (vermögen.isEmpty()) {
+				System.out.println("Der Held hat noch keine Schätze und kann daher auch nichts verlieren! Glück gehabt!");
+			} else {
+				// verlust des 1. Schatzes
+				Schatz verlust = vermögen.remove(0);
+				System.out.println("Der Held verliert eine Schatzkarte im Wert von " + verlust.getValue() + " Goldstücken");
+				monster.add(verlust);
+				if (vermögen.isEmpty()) {
+					System.out.println("Der Held hat noch keine Schätze und kann daher auch nichts verlieren! Glück gehabt!");
+				} else {
+					// verlust des 1. Schatzes
+					verlust = vermögen.remove(0);
+					System.out.println("Der Held verliert noch eine Schatzkarte im Wert von " + verlust.getValue() + " Goldstücken");
+					monster.add(verlust);
+				}
+
+			}
+			this.mussAussetzen(1);
+			break;
+
+		default:
+			// der Held ist tot
+			System.out.println("Leider har der Held den Kampf mit dem Monster verlohren und ist nun tot!");
+			this.stirbt();
+			break;
+		}
+		
+	}
 }
